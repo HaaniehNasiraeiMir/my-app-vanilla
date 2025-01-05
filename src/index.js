fa2e9dab007eb137d674f85983ca201b5da8ac46
Haanieh Nasiraei Mir <haaniehnasiraeimir@gmail.com>
2025-01-05 21:42:03 +0330

    added the current temp and city

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
+function updateWeatherInfo(response) {
+    let temperatureElement = document.querySelector("#weather-app-temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#weather-app-city");
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = Math.round(temperature);
+}
+function searchCity(city) {
+    let apiKey = "ffaeodfecfc1t6ea1f3ed031874f0dab";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiURL).then(updateWeatherInfo);
+}
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#weather-app-city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+searchCity("NewYork");
